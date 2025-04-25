f81be23d46cef72b678c211df999057e6ea1d1b7
Jason Hiatt <Atraxi@users.noreply.github.com>
2025-04-25 12:44:13 +1200

    Only include resin in voidcraft byproducts if resin is enabled in wooden industry

--- prototypes/compat/voidcraft.lua
+++ prototypes/compat/voidcraft.lua
@@ -4,9 +4,16 @@ if mods["Voidcraft"] then
   local woodchips_bad_product = mods["wood-industry"] and "woodchips" or "wood"
   local scrapamt = mods["wood-industry"] and 2 or 1
 
+  local basetbl
+  if mods["wood-industry"] and settings.startup["wood-industry-resin"].value then
+    basetbl = {"sulfuric-bacteria", woodchips_bad_product, "resin"}
+  else
+    basetbl = {"sulfuric-bacteria", woodchips_bad_product}
+  end
+
   data:extend({
     VOIDCRAFT.voidcraft_recipe(
     {prefix="__vulcanus-sulfuric-bacteria__", subgroup="voidcraft-vulcanus", img="sulfuric-stromatolite", localised_name="entity-name.sulfuric-stromatolite", amt2=scrapamt},
-      {"sulfuric-bacteria", woodchips_bad_product, "resin"}, "vpa-xbb", vcc.advanced_resource, "s6x-void-vulcanus", {0.7, 0.22, 0.6}),
+      basetbl, "vpa-xbb", vcc.advanced_resource, "s6x-void-vulcanus", {0.7, 0.22, 0.6}),
   })
 end
